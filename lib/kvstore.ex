79812f56a3f88efa10de68382f906c09c51446c6
lorens42rus <scroll42ru@gmail.com>
2019-06-23 20:15:30 +0700

    Added router to supervisor

--- lib/kvstore.ex
+++ lib/kvstore.ex
@@ -8,7 +8,8 @@ defmodule Kvstore.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      {Kvstore.Storage, name: Kvstore.Storage}
+      {Kvstore.Storage, name: Kvstore.Storage},
+      {Plug.Cowboy, scheme: :http, plug: Kvstore.Router, options: [port: 8080]},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
