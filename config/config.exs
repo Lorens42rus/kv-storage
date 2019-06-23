5d36852e35a310455a152d429d09a93877f2ac79
lorens42rus <scroll42ru@gmail.com>
2019-06-23 20:44:38 +0700

    Added ability to configure project port

--- config/config.exs
+++ config/config.exs
@@ -30,5 +30,6 @@ use Mix.Config
 #     import_config "#{Mix.env()}.exs"
 
 config :kvstore, storage_table_name: 'storage'
+config :kvstore, project_port: 8000
 
 import_config "#{Mix.env()}.exs"
--- lib/kvstore.ex
+++ lib/kvstore.ex
@@ -1,19 +1,16 @@
 defmodule Kvstore.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
+  @port Application.get_env(:kvstore, :project_port)
+
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
       {Kvstore.Storage, name: Kvstore.Storage},
-      {Plug.Cowboy, scheme: :http, plug: Kvstore.Router, options: [port: 8080]},
+      {Plug.Cowboy, scheme: :http, plug: Kvstore.Router, options: [port: @port]},
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Kvstore.Supervisor]
     Supervisor.start_link(children, opts)
   end
