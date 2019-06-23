edb31a72b8c1a1e4c20b67f27e01d405de170ec2
lorens42rus <scroll42ru@gmail.com>
2019-06-23 19:39:58 +0700

    Initial commit

---
+++ lib/kvstore.ex
@@ -0,0 +1,18 @@
+defmodule Kvstore do
+  @moduledoc """
+  Documentation for Kvstore.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kvstore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
